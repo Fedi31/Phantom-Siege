6f4940b77a60c6e8b1308a3cf7181e4700be9087
Federica <f.dimichele1@campus.uniurb.it>
2025-03-12 15:29:18 +0100

    implementazione iniziale Position

--- src/model/Position.java
+++ src/model/Position.java
@@ -2,5 +2,24 @@ package model;
 
 //Position rappresenta una singola posizione sulla mappa.
 public class Position {
+	    private int x;
+	    private int y;
 
+	    public Position(int x, int y) {
+	        this.x = x;
+	        this.y = y;
+	    }
+
+	    public int getX() {
+	        return x;
+	    }
+
+	    public int getY() {
+	        return y;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "Position(x=" + x + ", y=" + y + ")";
+	    }
 }
