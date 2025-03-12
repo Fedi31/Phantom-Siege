7337389ced5e329ec5955a0eb51d69adf9857dfd
Rossi1109 <andrearossi0904@gmail.com>
2025-03-12 16:40:42 +0100

    Add basic GUI implementation and update Main class to initialize it

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,10 @@
 package main;
 
+import view.Gui;
+
 public class Main {
-    public static void main(String[] args) {
-    	//ciao
+    public static void main(String[] args) 
+    {
+    	Gui prova = new Gui();
     }
 }
