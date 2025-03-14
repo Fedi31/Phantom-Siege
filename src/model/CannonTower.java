a6f9d867d913bffa84a17944b443fde9560f4a29
Federica <f.dimichele1@campus.uniurb.it>
2025-03-14 17:59:53 +0100

    update tower and ghosts classes

---
+++ src/model/CannonTower.java
@@ -0,0 +1,10 @@
+package model;
+
+public class CannonTower extends BasicTower{
+
+	public CannonTower(String name, int cost, int attackPower, int range) {
+		super(name, cost, attackPower, range);
+		// TODO Auto-generated constructor stub
+	}
+
+}
